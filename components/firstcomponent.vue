4d6f08ef93150be83685349e3f630a0c15edc037
uzair0331 <uxair0331xhaikhgmail.com>
2024-07-04 11:51:51 +0500

    up work raplica

---
+++ components/firstcomponent.vue
@@ -0,0 +1,3 @@
+<template> <h1 class=" text-center font-bold mt-4 text-xl">Welcome to 1st component</h1>
+
+</template>
--- components/navbar.vue
+++ components/navbar.vue
@@ -1,15 +1,56 @@
-<template><div >
-    <nav class="flex shadow-lg p-5 justify-center gap-6  bg-[#2a0040] text-[#f5f5f5] text-lg ">
-    <NuxtLink to="/" >Home</NuxtLink>
-    <NuxtLink to="/iphone">Iphones</NuxtLink>
-    <NuxtLink to="/airpods">AirPods</NuxtLink>
-    <NuxtLink to="/macbooks">Macbooks</NuxtLink>
-    <NuxtLink to="/totalcart">Total purchasing</NuxtLink>
-    <p>cart ({{ cart.length }})</p>
-  </nav>
-  </div></template>
-  <script setup>
-import { useCart } from '~/composable/states';
+<template>
+  <div>
+    <nav class="flex flex-wrap shadow-md p-5 justify-between gap-6 bg-[#ffffff] text-sm">
+      <div class="flex gap-6 items-center">
+        <img width="80" height="80" src="/images/upwork.svg" alt="macbook15" />
+        <div class="hidden md:flex gap-6">
+          <NuxtLink to="/">Find talent</NuxtLink>
+          <NuxtLink to="/">Find work</NuxtLink>
+          <NuxtLink to="/">Why Upwork</NuxtLink>
+          <NuxtLink to="/">What's New</NuxtLink>
+          <NuxtLink to="/">Enterprise</NuxtLink>
+        </div>
+      </div>
 
-const cart = useCart();
+      <button class="md:hidden" @click="toggleMenu">
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+        </svg>
+      </button>
+
+      <div class="hidden md:flex gap-6">
+        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="/">Iphones</NuxtLink>
+        <NuxtLink to="/">AirPods</NuxtLink>
+        <NuxtLink to="/">Macbooks</NuxtLink>
+        <NuxtLink to="/">Total Purchasing</NuxtLink>
+      </div>
+    </nav>
+
+    <div v-if="isMenuOpen" class="md:hidden flex flex-col gap-2 p-5 bg-[#ffffff] shadow-md">
+      <NuxtLink to="/">Find talent</NuxtLink>
+      <NuxtLink to="/">Find work</NuxtLink>
+      <NuxtLink to="/">Why Upwork</NuxtLink>
+      <NuxtLink to="/">What's New</NuxtLink>
+      <NuxtLink to="/">Enterprise</NuxtLink>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/">Iphones</NuxtLink>
+      <NuxtLink to="/">AirPods</NuxtLink>
+      <NuxtLink to="/">Macbooks</NuxtLink>
+      <NuxtLink to="/">Total Purchasing</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const isMenuOpen = ref(false);
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+};
 </script>
+
+<style scoped>
+</style>
